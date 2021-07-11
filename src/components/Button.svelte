8c9ba8cb310b1b1227b1bf017ee48b378672b203
javila35 <josepharthuravila@gmail.com>
2021-07-11 13:01:56 -0700

    Add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let genre;
+</script>
+
+<button>{genre}</button>
+
+<style>
+	button {
+		background-color: #fdb014;
+		border: 1px solid #c01b21;
+		border-radius: 16px;
+		width: 22.5em;
+		max-width: 360px;
+		height: 5.625em;
+		max-height: 90px;
+		font-size: 2.25em;
+		margin-top: 30px;
+		margin-bottom: 30px;
+	}
+</style>
