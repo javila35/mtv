d7005e6e07dbe86dc0ef60f3d489119e0b5f2a10
javila35 <josepharthuravila@gmail.com>
2021-07-11 13:02:25 -0700

    Get landing page to mvp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '../components/Button.svelte';
+	import Video from '../components/Video.svelte';
+</script>
+
+<div>
+	<Video />
+	<div class="button-area">
+		<Button genre="Country" />
+		<Button genre="Rap" />
+		<Button genre="Electronic" />
+		<Button genre="Rock" />
+	</div>
+</div>
+
+<style>
+	div {
+		display: grid;
+		max-height: 100vh;
+		grid-template: 1fr 3fr 1fr 1fr / 1fr;
+		grid-template-areas:
+			'header'
+			'button'
+			'search'
+			'footer';
+	}
+
+	div.button-area {
+		grid-area: button;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
